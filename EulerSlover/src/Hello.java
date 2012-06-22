cefa40958e202e261a4b28b180aa7c3d81b44080
adamzhangjie <adamzhangjie@gmail.com>
2012-06-22 14:14:46 +0800

    作为一个合理的修改上传，其实是进行测试的文件

--- EulerSlover/src/Hello.java
+++ EulerSlover/src/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is just show you what i mean");
+		// this is the end of program
 
 	}
 
